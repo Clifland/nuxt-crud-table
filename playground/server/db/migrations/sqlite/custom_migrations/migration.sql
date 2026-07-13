77a2056613a4e44a9cad107a17908df8691f7583
Cliford Pereira <cliford.pereira@gmail.com>
2026-07-13 07:48:43 +0530

    chore: add custom migrations for managing stock/inventory

--- playground/server/db/migrations/sqlite/custom_migrations/migration.sql
+++ playground/server/db/migrations/sqlite/custom_migrations/migration.sql
@@ -6,3 +6,27 @@ BEGIN
   SET price = (SELECT price FROM products WHERE id = NEW.product_id)
   WHERE id = NEW.id;
 END;
+
+CREATE TRIGGER products_set_stock_insert
+AFTER INSERT ON orderitems
+BEGIN
+  UPDATE products
+  SET stock = stock - NEW.quantity
+  WHERE id = NEW.product_id;
+END;
+
+CREATE TRIGGER products_set_stock_delete
+AFTER DELETE ON orderitems
+BEGIN
+  UPDATE products
+  SET stock = stock + OLD.quantity
+  WHERE id = OLD.product_id;
+END;
+
+CREATE TRIGGER products_set_stock_update
+AFTER UPDATE ON orderitems
+BEGIN
+  UPDATE products
+  SET stock = stock + OLD.quantity - NEW.quantity
+  WHERE id = NEW.product_id;
+END;
