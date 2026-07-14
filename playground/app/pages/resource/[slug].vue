4c376d60399dfcfcf049c919d7d833d719b80a3e
Cliford Pereira <cliford.pereira@gmail.com>
2026-07-14 21:51:51 +0530

    chore: make print working for multiple template

--- playground/app/pages/resource/[slug].vue
+++ playground/app/pages/resource/[slug].vue
@@ -1,15 +1,25 @@
 <script setup lang="ts">
 const route = useRoute()
 const resource = route.params.slug as string
+import InvoiceTemplate from '~/components/InvoiceTemplate.vue'
+// import ShippingLabel from '~/components/ShippingLabel.vue'
+
+const templateMap: Record<string, Component> = {
+  orderitems: InvoiceTemplate,
+  // shipments: ShippingLabel,
+}
 </script>
 
 <template>
   <div v-if="resource">
     <NctCrudTable :resource="resource">
-      <template #print-template="slotProps">
-        <InvoiceTemplate v-bind="slotProps" />
-      </template>
-    </NctCrudTable>
+        <template #print-template="slotProps">
+          <component
+            :is="templateMap[slotProps.resource!]"
+            v-bind="slotProps"
+          />
+        </template>
+      </NctCrudTable>
   </div>
   <div
     v-else
